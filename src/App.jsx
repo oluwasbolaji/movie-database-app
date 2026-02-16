5e49f2f9bbb0625c398316a1487c6751a137f353
Oluwasola Bolaji <oluwasbolaji1993@gmail.com>
2026-02-17 00:35:09 +0100

    Week 4: Connected OMDB API and implemented search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,44 @@
+import { useState } from "react"
 import MovieList from "./components/MovieList"
+import { searchMovies } from "./services/omdbApi"
 
 function App() {
-  const sampleMovies = [
-    {
-      imdbID: "tt1375666",
-      Title: "Inception",
-      Year: "2010",
-      Poster: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
-    },
-    {
-      imdbID: "tt0816692",
-      Title: "Interstellar",
-      Year: "2014",
-      Poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
-    },
-    {
-      imdbID: "tt0468569",
-      Title: "The Dark Knight",
-      Year: "2008",
-      Poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-    },
-  ]
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState("")
+
+  const handleSearch = async () => {
+    const results = await searchMovies(query)
+    setMovies(results)
+  }
 
   return (
     <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-6">Movie Database</h1>
 
-      <MovieList movies={sampleMovies} />
+      <h1 className="text-3xl font-bold mb-6">
+        Movie Database App
+      </h1>
+
+      {/* Search Bar */}
+      <div className="flex gap-2 mb-6">
+        <input
+          type="text"
+          placeholder="Search movies..."
+          className="border p-2 rounded w-full"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        <button
+          onClick={handleSearch}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Search
+        </button>
+      </div>
+
+      {/* Movie List */}
+      <MovieList movies={movies} />
+
     </div>
   )
 }
--- src/services/omdbApi.js
+++ src/services/omdbApi.js
@@ -1,21 +1,20 @@
 const API_KEY = "c1eebcc9"
 
 export const searchMovies = async (query) => {
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
-  )
+  try {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`
+    )
 
-  const data = await response.json()
+    const data = await response.json()
 
-  return data.Search || []
-}
-
-export const getMovieDetails = async (id) => {
-  const response = await fetch(
-    `https://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`
-  )
-
-  const data = await response.json()
-
-  return data
+    if (data.Response === "True") {
+      return data.Search
+    } else {
+      return []
+    }
+  } catch (error) {
+    console.error("Error fetching movies:", error)
+    return []
+  }
 }
